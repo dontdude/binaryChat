6ff0370f1581ae1c5c5fa006a9c1f2b736b6179e
Chandan Mishra <imchandan1947@gmail.com>
2023-08-07 20:21:50 +0530

    First commit

---
+++ app/(site)/page.tsx
@@ -0,0 +1,18 @@
+export default function Home() {
+  return (
+    <div
+     className="
+       flex
+       min-h-full
+       flex-col
+       justify-content
+       py-12
+       sm:px-6
+       lg:px-6
+       bg-gray-100
+      "
+    >
+     <div className="sm:mx-auto sm:w-full sm:max-w-md"></div>
+    </div>
+  )
+}
