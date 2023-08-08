706dd77b72ee0751d3e3bf5c8e743a0fdcdc28ae
Chandan Mishra <imchandan1947@gmail.com>
2023-08-08 23:51:28 +0530

    index page

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,18 +1,44 @@
-export default function Home() {
+import Image from "next/image";
+//import AuthForm from "./components/AuthForm";
+
+const Auth = () => {
   return (
-    <div
-     className="
-       flex
-       min-h-full
-       flex-col
-       justify-content
-       py-12
-       sm:px-6
-       lg:px-6
-       bg-gray-100
+    <div 
+      className="
+        flex 
+        min-h-full 
+        flex-col 
+        justify-center 
+        py-12 
+        sm:px-6 
+        lg:px-8 
+        bg-gray-100
       "
     >
-     <div className="sm:mx-auto sm:w-full sm:max-w-md"></div>
-    </div>
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <Image
+          height="52"
+          width="52"
+          className="mx-auto w-auto"
+          src="/images/Logo.png"
+          alt="Logo"
+        />
+        <h2 
+          className="
+            mt-6 
+            text-center 
+            text-2xl 
+            font-bold 
+            tracking-tight 
+            text-gray-900
+          "
+          >
+            Sign in to your account
+        </h2>
+      </div>
+      {/* <AuthForm />       */}
+  </div>
   )
 }
+
+export default Auth;
